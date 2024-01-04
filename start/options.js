9146ee891db5fcf1af301b4ea0cfc5dfa68da450
ueharaneal <ueharaneal@gmail.com>
2024-01-03 23:41:55 -0800

    options

--- start/options.js
+++ start/options.js
@@ -1,17 +1,18 @@
-const inputElement = document.getElementById("name-input");
-const saveButton = document.getElementById("save-btn");
+const inputElement = document.getElementById("name-input")
+const saveButton = document.getElementById("save-btn")
 
 saveButton.addEventListener("click", () => {
-  const name = inputElement.value;
-  chrome.storage.sync.set(
-    {
-      name: name,
-    },
-    () => {
-      console.log(`Name set to ${name}`);
-    }
-  );
-});
-chrome.storage.sync.get(["name"], (res) => {
-  inputElement.value = res.name;
-});
+	const name = inputElement.value
+	chrome.storage.sync.set(
+		{
+			name: name,
+		},
+		() => {
+			console.log(`Name set to ${name}`)
+		}
+	)
+})
+chrome.storage.sync.get(["name"], res => {
+	inputElement.value = res.name
+})
+//
